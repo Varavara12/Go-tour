768ec954e885939cae4e0c8400089f2204ea4c11
Vitalii <narodnyjpikap@gmail.com>
2019-10-30 10:13:11 +0200

    tour-map

--- js/main.js
+++ js/main.js
@@ -6,15 +6,30 @@ $(function(){
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="../img/arrows-left.svg" alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="../img/arrows-right.svg" alt="">',
-        asNavFor: '.slider-dots',
+        asNavFor: '.slider-dotshead',
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
 
-
     });
+
+    $('.tour__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="../img/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="../img/arrows-right.svg" alt="">',
+        asNavFor: '.slider-map',
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.tour__slider',
+        focusOnSelect: true
+    })
 
 });
